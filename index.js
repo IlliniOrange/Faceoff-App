b2ff901a4f84392fe3b126b7286c44600b69ac00
IlliniOrange <joepecora2@gmail.com>
2024-10-08 00:45:35 -0600

    Updating UI and adding total calcs

--- index.js
+++ index.js
@@ -1,18 +1,55 @@
 // Initialize variables
-
+// Document variables
 const totalWonEl = document.getElementById("totalWon-el")
-const winButtonEl = docuent.getElementById("winButton-el")
+const totalLostEl = document.getElementById("totalLost-el")
+const winButtonEl = document.getElementById("winButton-el")
+const loseButtonEl = document.getElementById("loseButton-el")
+const totalFaceoffsEl = document.getElementById("totalFaceoffs-el")
+const winPercentEl = document.getElementById("winPercent-el")
 
+// Scorekeeping variables
+let totalWon = 0
+let totalLost = 0
+let totalFaceoffs = 0
+let winPercent = 0
+
+// Testing Stuff
+    console.log(Date())
+
+// Event Listeners
+
+winButtonEl.addEventListener("click", function() {
+    addTotalWon()
+})
+
+loseButtonEl.addEventListener("click", function() {
+    addTotalLost()
+})
 
 // Scorekeeping functions
 
 function addTotalWon() {
     totalWon += 1
     totalWonEl.textContent = totalWon
+    totalFaceoffs += 1
+    calcWinPercent()
 }
 
-// Listeners
+function addTotalLost() {
+    totalLost += 1
+    totalLostEl.textContent = totalLost
+    totalFaceoffs += 1
+    calcWinPercent()
+}
 
-winButtonEl.addEventListener("click", function() {
-    addTotalWon()
-})
+// Calculates the win percentage
+function calcWinPercent() {
+    winPercent = totalWon / totalFaceoffs
+    winPercentEl.textContent = formatPercentage(winPercent)
+    totalFaceoffsEl.textContent = totalFaceoffs
+}
+
+// Format the win percentage
+function formatPercentage(num) {
+  return (num * 100).toFixed(2) + '%';
+}
