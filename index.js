95f8b95447959c14acc3838b5b7ea4f242016c39
IlliniOrange <joepecora2@gmail.com>
2024-10-09 23:03:45 -0600

    Game object and local strorage
    Local storage used to maintain current game stats in the event of a browser refresh.
    
    Created game object to replace separate variables to streamline saving to local storage.

--- index.js
+++ index.js
@@ -4,17 +4,34 @@ const totalWonEl = document.getElementById("totalWon-el")
 const totalLostEl = document.getElementById("totalLost-el")
 const winButtonEl = document.getElementById("winButton-el")
 const loseButtonEl = document.getElementById("loseButton-el")
+const clearButtonEl = document.getElementById("clearButton-el")
 const totalFaceoffsEl = document.getElementById("totalFaceoffs-el")
 const winPercentEl = document.getElementById("winPercent-el")
+const opponentEl = document.getElementById("opponent-el")
 
-// Scorekeeping variables
-let totalWon = 0
-let totalLost = 0
-let totalFaceoffs = 0
-let winPercent = 0
+// Create game object
+
+let game = {
+    date: Date(),
+    opponent: "Judge",
+    faceOffsWon: 0,
+    faceOffsLost: 0,
+    totalFaceOffs: 0,
+    winPercent: 0,
+}
+
+// Load game if it exists in local storage, otherwise initialize an object
+
+let ls = localStorage.getItem("game")
+if (ls) {
+    game = JSON.parse(ls)
+} else {
+    opponentEl.innerHTML = "<input type='text' value='Input opponent name'><button class='inputButton'>Button</button>"
+}
+
 
 // Testing Stuff
-    console.log(Date())
+
 
 // Event Listeners
 
@@ -26,30 +43,54 @@ loseButtonEl.addEventListener("click", function() {
     addTotalLost()
 })
 
+clearButtonEl.addEventListener("click", function() {
+    if (confirm("Are you sure? This will delete the current game")) {
+        localStorage.removeItem("game")
+        location.reload()
+    }
+})
+
+// Save game to local storage
+
+function saveGame() {
+    localStorage.setItem("game", JSON.stringify(game))
+}
+
 // Scorekeeping functions
 
 function addTotalWon() {
-    totalWon += 1
-    totalWonEl.textContent = totalWon
-    totalFaceoffs += 1
+    game.faceOffsWon += 1
+    game.totalFaceOffs +=1
     calcWinPercent()
+    drawUI()
 }
 
 function addTotalLost() {
-    totalLost += 1
-    totalLostEl.textContent = totalLost
-    totalFaceoffs += 1
+    game.faceOffsLost += 1
+    game.totalFaceOffs += 1
     calcWinPercent()
+    drawUI()
 }
 
 // Calculates the win percentage
 function calcWinPercent() {
-    winPercent = totalWon / totalFaceoffs
-    winPercentEl.textContent = formatPercentage(winPercent)
-    totalFaceoffsEl.textContent = totalFaceoffs
+    game.winPercent = game.faceOffsWon / game.totalFaceOffs
+    drawUI()
 }
 
 // Format the win percentage
 function formatPercentage(num) {
-  return (num * 100).toFixed(2) + '%';
+    return (num * 100).toFixed(2) + '%';
+  }
+
+// Draw the UI
+function drawUI() {
+    totalWonEl.textContent = game.faceOffsWon
+    totalLostEl.textContent = game.faceOffsLost
+    totalFaceoffsEl.textContent = game.totalFaceOffs
+    winPercentEl.textContent = formatPercentage(game.winPercent)
+    saveGame()
 }
+
+// Start
+drawUI() // Draws UI from loaded game, otherwise saves a blank game object in local storage
