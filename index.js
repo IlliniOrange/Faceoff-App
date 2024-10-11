c29f7dc4f342a9f58a21ed1fb614dab1538ffda5
IlliniOrange <joepecora2@gmail.com>
2024-10-10 22:29:09 -0600

    Save game info to Domo

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 // Initialize variables
-// Document variables
+
 const totalWonEl = document.getElementById("totalWon-el")
 const totalLostEl = document.getElementById("totalLost-el")
 const winButtonEl = document.getElementById("winButton-el")
 const loseButtonEl = document.getElementById("loseButton-el")
-const clearButtonEl = document.getElementById("clearButton-el")
+const newGameButtonEl = document.getElementById("newGameButton-el")
 const totalFaceoffsEl = document.getElementById("totalFaceoffs-el")
 const winPercentEl = document.getElementById("winPercent-el")
 const opponentEl = document.getElementById("opponent-el")
+const saveButtonEl = document.getElementById("saveButton-el")
 
 // Create game object
 
@@ -20,18 +21,14 @@ let game = {
     winPercent: 0,
 }
 
-// Load game if it exists in local storage, otherwise initialize an object
+// If a game exists in local storage, load it to the game object. Otherwise set the state for a new game entry.
 
 let ls = localStorage.getItem("game")
 if (ls) {
     game = JSON.parse(ls)
 } else {
-    opponentEl.innerHTML = "<input type='text' value='Input opponent name'><button class='inputButton'>Button</button>"
+    opponentEl.innerHTML = "<input type='text' id='opponentInput' value='Input opponent name'><button id='inputButton-el' class='inputButton' onclick=getOpponent()>Button</button>"
 }
-
-
-// Testing Stuff
-
 
 // Event Listeners
 
@@ -43,12 +40,21 @@ loseButtonEl.addEventListener("click", function() {
     addTotalLost()
 })
 
-clearButtonEl.addEventListener("click", function() {
+newGameButtonEl.addEventListener("click", function() {
     if (confirm("Are you sure? This will delete the current game")) {
         localStorage.removeItem("game")
         location.reload()
     }
 })
+
+saveButtonEl.addEventListener("click", function() {
+    if (confirm("Are you sure? This will save the current game")) {
+        writeDataToDomo(game)
+    }
+})
+
+// TESTING
+
 
 // Save game to local storage
 
@@ -72,7 +78,6 @@ function addTotalLost() {
     drawUI()
 }
 
-// Calculates the win percentage
 function calcWinPercent() {
     game.winPercent = game.faceOffsWon / game.totalFaceOffs
     drawUI()
@@ -92,5 +97,60 @@ function drawUI() {
     saveGame()
 }
 
+/* This section contains functions used to write the data to Domo through OAuth API
+    - For now, not expecting to save data in less than 1 hour intervals, the code will refresh
+    the token on each call */
+    
+// Get an access token
+ async function getAccessToken() {
+    const url = 'https://api.domo.com/oauth/token?grant_type=client_credentials';
+    const clientId = '6b23c9b5-fb97-464a-8c51-2ec477cfac47'
+    const clientSecret = '1790fd9c39c979cf4d400576ccf162b7437b6139bd754c5a37037f9e6b5f46e6'
+    const authorizationValue = 'Basic ' + btoa( clientId + ':' + clientSecret )
+    
+    const options = {
+      method: 'GET',
+      headers: {
+        Accept: 'application/json',
+        Authorization: authorizationValue,
+        grant_type: 'data',
+      }
+    };
+    
+    try {
+        const response = await fetch(url, options);
+        const data = await response.json();  
+        return await data.access_token
+    } catch (error) {
+        console.error(error);
+    }
+ }
+
+ async function writeDataToDomo (game) {
+    const url = 'https://api.domo.com/v1/datasets/497a5fdd-17a6-4ec7-b0d2-1298446c55a0/data?updateMethod=APPEND';
+    const authorizationValue = 'Bearer ' + await getAccessToken()
+    const gameString = game.date + "," + game.opponent + "," + game.faceOffsWon + "," + game.faceOffsLost + "," + game.totalFaceOffs + "," + game.winPercent
+    const options = {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'text/csv',
+            Accept: 'application/json',
+            Authorization: authorizationValue,
+        },
+        body: gameString
+    };
+
+    try {
+        const response = await fetch(url, options);
+        const data = await response.json();
+        console.log(response)
+    } catch (error) {
+        // console.error(error);
+    }
+
+    }
+
 // Start
 drawUI() // Draws UI from loaded game, otherwise saves a blank game object in local storage
+
+// console.log(await getAccessToken())
